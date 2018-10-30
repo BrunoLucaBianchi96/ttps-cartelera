bd8868d57b7524ca1764158817393f8bf6792258
Juan Ferrari <juannferrari@gmail.com>
2018-10-30 19:13:31 -0300

    permission model

--- cartelera/src/main/java/model/Permission.java
+++ cartelera/src/main/java/model/Permission.java
@@ -1,4 +1,31 @@
 package model;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "permission")
 public class Permission {
+
+    @Id
+    @Column(name = "permission_id")
+    @GeneratedValue(generator = "incrementor")
+    @GenericGenerator(name = "incrementor", strategy = "increment")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id;    }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public Permission(){
+
+    }
 }
