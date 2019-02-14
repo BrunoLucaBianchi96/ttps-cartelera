2aaf26ff1cb00d092e1f6d0b18fdff33da9ed411
Bruno Bianchi <brunolucabianchi1996@gmail.com>
2019-02-14 18:05:15 -0300

    added admin check

--- cartelera/src/main/java/spring/config/controllers/CarteleraController.java
+++ cartelera/src/main/java/spring/config/controllers/CarteleraController.java
@@ -1,10 +1,12 @@
 package spring.config.controllers;
 
 import model.Billboard;
+import model.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import spring.config.services.BillboardService;
 import spring.config.services.TokenService;
+import spring.config.services.UserService;
 import utils.BillboardMarshaller;
 
 import java.util.List;
@@ -19,6 +21,9 @@ public class CarteleraController {
     @Autowired
     private BillboardService billboardService;
 
+    @Autowired
+    private UserService userService;
+
     private BillboardMarshaller billboardMarshaller = new BillboardMarshaller();
 
     @PostMapping("/cartelera")
@@ -30,10 +35,22 @@ public class CarteleraController {
         if (!this.tokenService.checkIfExists(token, user_id)) {
             return "401 - Token mismatch";
         }
+        if(!isAdmin(user_id)){
+            return "401 - not Allowed";
+        }
 
         Billboard billboard = this.billboardMarshaller.toObject(json);
         billboardService.save(billboard);
         return json;
+    }
+
+    private boolean isAdmin(int user_id) {
+        List<Role> roles=this.userService.getUserById(user_id).getRoles();
+        boolean isAdmin = false;
+        for( Role each : roles){
+            isAdmin = (each.getName().equals("ADMIN") || isAdmin);
+        }
+        return isAdmin;
     }
 
     @GetMapping("/cartelera")
